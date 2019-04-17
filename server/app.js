640953e9bb8e1de5543253e0d8993e639603b8a6
Bingyao Wang <bingyaowang.nj@gmail.com>
2019-04-17 15:06:11 -0400

    set up router

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const todosRouter = require('./routes/todos');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/todos', todosRouter);
 app.use('/users', usersRouter);
 
 // Render React page
